bfacfb1f0fda1a9b50c160567e14db3b92aa1995
mbuyankin <mbuyankin@gmail.com>
2023-05-25 09:31:14 +0300

    Level 14

---
+++ src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int x) {
+    }
+
+    public static void print(int x, int y) {
+    }
+
+    public static void print(int x, int y, int z) {
+    }
+
+    public static void print(int x, int y, int z, int s) {
+    }
+
+    public static void print(String s) {
+    }
+
+}
